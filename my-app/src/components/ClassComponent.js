ffa6a8ee20e8858fa530b4a29e38932b552b9918
nasifahmedoney <nasif.ahmed@live.com>
2022-08-05 02:29:16 +0600

    using function

--- my-app/src/components/ClassComponent.js
+++ my-app/src/components/ClassComponent.js
@@ -2,11 +2,28 @@ import React,{Component} from "react";
 
 class ClassComponent extends Component{
     constructor(props){
-        super(props)
+        super(props);
+        this.state = {
+            users:[
+                {name:'user1', age:'100'},
+                {name:'user2', age:'200'},
+                {name:'user3', age:'300'}
+            ]
+        }
+    }
+    clickHandler() {
+        console.log("hello");
+        alert('alart');
     }
     render(){
         return (
-            <h1>hello {this.props.name}</h1>
+            <div>
+                <button onClick={this.clickHandler}>Click here</button>
+                <h1>hello {this.state.users[0].name}</h1>
+                <h1>hello {this.state.users[1].name}</h1>
+                <h1>hello {this.state.users[2].name}</h1>
+            </div>
+            
         );
     }
 }
