1d8e2bb1d210c6fd594b0defaf6ebee56ac14e0a
nasifahmedoney <nasif.ahmed@live.com>
2022-08-15 17:47:58 +0600

    using bootstrap
    remove body css from index.css,
    import css in index.js:
    import 'bootstrap/dist/css/bootstrap.min.css';

---
+++ my-app/src/header.jsx
@@ -0,0 +1,36 @@
+import { Component } from "react";
+//import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+//import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+//import NavDropdown from 'react-bootstrap/NavDropdown';
+
+class header extends Component{
+
+    render(){
+        return(
+            <div>
+                <Navbar bg="light" expand="lg">
+                <Container fluid>
+                <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+                <Navbar.Toggle aria-controls="navbarScroll" />
+                <Navbar.Collapse id="navbarScroll">
+                <Nav
+                    className="me-auto my-2 my-lg-0"
+                    style={{ maxHeight: '100px' }}
+                    navbarScroll
+                >
+                    <Nav.Link href="#action1">Home</Nav.Link>
+                    <Nav.Link href="#action2">Link</Nav.Link>
+                </Nav>
+                
+                </Navbar.Collapse>
+            </Container>
+            </Navbar>
+            </div>
+        );
+    }
+}
+
+export default header;
