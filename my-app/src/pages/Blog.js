5ed35193a00d18d55f10b6708bd132cc8f8f6334
nasifahmedoney <nasif.ahmed@live.com>
2022-08-21 03:01:27 +0600

    loading data from api, using axios and state

--- my-app/src/pages/Blog.js
+++ my-app/src/pages/Blog.js
@@ -1,6 +1,25 @@
+import axios from 'axios';
+import { useState } from 'react';
+
 function Blog(){
-    return(
-        <div>Blog Page</div>
-    );
+    const [ data, setNewData ] = useState([]);
+
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+    .then(loadeddata=>{
+        setNewData(loadeddata.data)
+    })
+    .catch(error=>{
+        console.log(error)
+    })
+    //console.log(data);
+        return(
+            <ul>
+                {data.map(posts=>{
+                    return(
+                        <li>{posts.title}</li>
+                    );
+                })}
+            </ul>
+        );
 }
 export default Blog;
