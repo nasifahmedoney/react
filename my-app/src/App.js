a6c0edc77e2919125416d290434c4beba80d4254
nasifahmedoney <nasif.ahmed@live.com>
2022-08-22 01:50:27 +0600

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,20 @@
 // import logo from './logo.svg';
 import React from 'react';
 import './App.css';
-import FormData from './components/formData';
-
+// import FormData from './components/formData';
+import {useState} from 'react';
 function App() {
+
+  let [count, setCount] = useState(0);
     return (
-      <div className="App">
-        <FormData />
+      <div>
+        {/* <FormData /> */}
+        <h1>Click: {count}</h1>
+        <button onClick={
+          ()=>{
+          setCount(count+1)
+          }
+        }>Click</button>
       </div>
     );
 }
