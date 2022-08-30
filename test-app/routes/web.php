4c7f011e9df44ed2a9e5e9d47728402b89d43fcc
nasifahmedoney <nasif.ahmed@live.com>
2022-08-31 00:10:31 +0600

    middleware intro
    php artisan make:middleware Check
    goto app/http/Kernel->add the midleware with any name,
    import the middleware in Kernel,
    add conditions in Check.php, add middleware in route

--- test-app/routes/web.php
+++ test-app/routes/web.php
@@ -18,4 +18,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/contact',[ContactController::class,'index']);
+Route::get('/contact',[ContactController::class,'index'])->middleware('checkValue');
